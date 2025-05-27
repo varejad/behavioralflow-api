7e92820c91c84f9fa9bb5f16692f033cc6cb3e61
varejad <junior18ademilson@gmail.com>
2025-05-27 15:39:23 -0300

    mudança nos dados passados pelo /estate

--- simulation.py
+++ simulation.py
@@ -28,4 +28,5 @@ class Simulation:
     
     # Retorna o JSON com todos os dados a serem desenhados no canvaJS
     def get_states(self):
-        return [{ "name": a.name, "state": a.state } for a in self.agents]
+        return[{"id": 1, "x": 100, "y": 150}, {"id": 2, "x": 300, "y": 250}]
+        #return [{ "name": a.name, "state": a.state } for a in self.agents]
